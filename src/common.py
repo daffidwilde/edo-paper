737736653fdbf281b0f221b369a7088c82a92ea3
Henry Wilde <henrydavidwilde@gmail.com>
2020-08-14 16:45:09 +0100

    Rerun experiments (#38)
    * Format and clean old source code.
    
    * Add new experiment scripts and environment.
    
    * Add temporary plots notebook.
    
    * Update edolab requirement.
    
    * Update epsilon in fitness functions.
    
    Formative analysis with kNN distance plots has found 0.14 to be a good
    choice for epsilon.
    
    * Add root variable to each exp. script.
    
    For the new `edolab` version.
    
    * Increase number of iterations to 100.
    
    * Update plots notebook with new data.
    
    * Add in contour plot.
    
    * Remove old plots.
    
    * Add new plots.

---
+++ src/common.py
@@ -0,0 +1,27 @@
+""" Common functions and parameters amongst the experiments. """
+
+from edo.distributions import Uniform
+from sklearn.preprocessing import MinMaxScaler
+
+
+def scale_dataframe(individual):
+    """ Scale the individual's dataframe to the unit square for calculating
+    fitness. """
+
+    original = individual.dataframe.copy()
+    dataframe = MinMaxScaler().fit_transform(original)
+
+    return dataframe
+
+
+size = 100
+row_limits = [50, 100]
+col_limits = [2, 2]
+max_iter = 100
+best_prop = 0.1
+mutation_prob = 0.01
+
+Uniform.param_limits["bounds"] = [0, 1]
+distributions = [Uniform]
+
+root = "../data/"
---
+++ src/kmeans_preferable.py
@@ -0,0 +1,58 @@
+""" Experiment script for k-means preferred to DBSCAN w.r.t. silhouette. """
+
+from sklearn.cluster import DBSCAN, KMeans
+from sklearn.metrics import silhouette_score
+
+import common
+
+
+def fitness(individual, n_clusters=3, eps=0.14, min_samples=5, n_seeds=5):
+    """ Cluster the data into `n_clusters` parts with k-means across `n_seeds`
+    runs, and with DBSCAN using `eps` and `min_samples`. Find the difference in
+    their silhouette scores and return the largest. """
+
+    dataframe = common.scale_dataframe(individual)
+
+    db = DBSCAN(eps=eps, min_samples=min_samples).fit(dataframe)
+    outlier_mask = db.labels_ == -1
+    labels = db.labels_[~outlier_mask]
+    if len(set(labels)) > 1:
+        data = dataframe[~outlier_mask, :]
+        db_score = silhouette_score(data, labels)
+    else:
+        db_score = None
+
+    differences = []
+    for seed in range(n_seeds):
+        km = KMeans(n_clusters, random_state=seed).fit(dataframe)
+        km_score = silhouette_score(dataframe, km.labels_)
+
+        if db_score is not None:
+            differences.append(km_score - db_score)
+        else:
+            differences.append(-3)
+
+    return max(differences)
+
+
+size = common.size
+row_limits = common.row_limits
+col_limits = common.col_limits
+max_iter = common.max_iter
+best_prop = common.best_prop
+mutation_prob = common.mutation_prob
+maximise = True
+
+distributions = common.distributions
+
+root = common.root
+
+__all__ = [
+    "size",
+    "row_limits",
+    "col_limits",
+    "max_iter",
+    "best_prop",
+    "mutation_prob",
+    "distributions",
+]
